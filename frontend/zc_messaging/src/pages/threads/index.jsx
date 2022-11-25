bd14f1bce45a57b229f75f9dbe9290708f264d8e
Harrylever <ukanah15thdean@gmail.com>
2022-11-25 09:07:45 +0100

    Feat-Create-Thread-Component

--- frontend/zc_messaging/src/pages/threads/index.jsx
+++ frontend/zc_messaging/src/pages/threads/index.jsx
@@ -1,13 +1,17 @@
 import { Helmet } from "react-helmet"
 import generatePageTitle from "../../utils/generatePageTitle"
 
+import styled from "styled-components"
+
 const Threads = () => {
   return (
     <>
       <Helmet>
         <title>{generatePageTitle("threads")}</title>
       </Helmet>
-      <h1>Threads Nocedldfdsfsdf one</h1>
+      <div>
+        <h1>This is the threads page</h1>
+      </div>
     </>
   )
 }
